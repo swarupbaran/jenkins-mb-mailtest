7ab5520871de54fa2f27a2f524102191bcc1a3f7
kanth0447 <sreekanthtagirise@gmail.com>
2020-09-28 19:00:11 +0530

    script execution permissions

--- status.sh
+++ status.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-#success_failure=${currentBuild.currentResult}
-#echo $success_failure
-#echo "Hello"
-#echo """${env.currentBuild.currentResult}"""
-#echo """${currentBuild.currentResult}"""
-#echo $success_failure
-#echo """${env.JOB_BASE_NAME}"""
-#echo """${JOB_BASE_NAME}"""
-
+file_name=$(echo $RANDOM)
+echo $file_name
 success_failure=$1
 job_base_name=$2
 
