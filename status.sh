2ac68c412ccc0abe49305535a5a8381ff14f53dc
kanth0447 <sreekanthtagirise@gmail.com>
2020-09-29 11:44:57 +0530

    script execution permissions

--- status.sh
+++ status.sh
@@ -6,17 +6,17 @@ file_name=$3
 echo $success_failure
 echo $job_base_name
 echo $file_name
-{
-echo "<!DOCTYPE html>"
-echo "<html>"
-echo "<head>"
-echo "<title>Page Title</title>"
-echo "</head>"
-echo "<body>"
-
-echo "<h1>My First Heading</h1>"
-echo "<p>My first paragraph.</p>"
-
-echo "</body>"
-echo "</html>"
-} > ${file_name}.html
+#{
+#echo "<!DOCTYPE html>"
+#echo "<html>"
+#echo "<head>"
+#echo "<title>Page Title</title>"
+#echo "</head>"
+#echo "<body>"
+#
+#echo "<h1>My First Heading</h1>"
+#echo "<p>My first paragraph.</p>"
+#
+#echo "</body>"
+#echo "</html>"
+#} > ${file_name}.html
