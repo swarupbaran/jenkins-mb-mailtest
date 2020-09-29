0407b7927ea6d97ce2af44ec376a41dfbc25c089
kanth0447 <sreekanthtagirise@gmail.com>
2020-09-29 11:52:14 +0530

    script execution permissions

--- status.sh
+++ status.sh
@@ -3,7 +3,7 @@
 success_failure=$1
 job_base_name=$2
 filename=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-export file_name = $filename
+export file_name=$filename
 echo $success_failure
 echo $job_base_name
 echo $file_name
